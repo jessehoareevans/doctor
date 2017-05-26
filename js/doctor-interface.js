0570e00ae76ebab6100a172edebcf4dc480f6ba5
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-05-26 10:37:15 -0700

    basic logic

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,1 +1,17 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
+
+var docInfo = function(doctors) {
+
+  doctors.forEach(function(doctor){
+    $('.output').append("<li>The title of the missing bike is " + bike.title + "the color is " + bike.color  + " the year of the bike is" + bike.year + ".</li>");
+});
+};
+
+$(document).ready(function(){
+  var currentDoctorObject = new Doctor();
+  $ ("#submit").click(function(){
+    var symptom = $('#symptom').val();
+    $('#symptom').val("");
+    currentDoctorObject.getDoctor();
+  });
+});
--- js/doctor.js
+++ js/doctor.js
@@ -1,1 +1,26 @@
 var apiKey = require('./../.env').apiKey;
+
+function Doctor(symptom){
+  this.symptom = symptom;
+}
+
+Doctor.prototype.getDoctor = function(medicalIssue, doctorInfo){
+  var doctors = [];
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      console.log(result);
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
+    for (var i=0; i < 20; i++) {
+      var name = result.practices[i].name;
+      var street = result.visit_address[i].street;
+      var zip = result.visit_address[i].zip;
+      newDoctor = new Doctor(name, street, zip);
+      doctors.push(newDoctor);
+    }
+    doctorInfo(doctors);
+};
+
+exports.doctorModule = Doctor;
