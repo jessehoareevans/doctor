7c398e40eef890e7be0c9df2a3e0642646766cbc
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-05-26 14:41:47 -0700

    add styling

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -3,7 +3,7 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 var doctorInfo = function(doctors) {
 
   doctors.forEach(function(doctor){
-    $('.output').append("<li>This is the name of a doctor that can help " + doctor.first + " " + doctor.last + " " + doctor.title  +  ", now quick bit of information about " + doctor.bio +  ".</li>");
+    $('.output').append("<li>This is the name of a doctor that can help " + doctor.first + " " + doctor.last + " " + doctor.title  +  ", now quick bit of information about " + doctor.bio +  "</li>");
   });
 };
 
